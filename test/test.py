64124d11cf82d84c3a1a1dab23509392c4db7779
Gleb <gleb19960@yandex.ru>
2017-11-21 22:09:37 +0300

    modifing test

--- test/test.py
+++ test/test.py
@@ -8,20 +8,20 @@ from flask import Flask
 app = Flask(__name__)
 class FlaskTests(unittest.TestCase):
 	def setUp(self):
+		p = subprocess.Popen('python ../chat.py', shell = True)
 		self.app = app.test_client()
 		self.app.testing = True
 		
 
 	def test_home_status_code(self):
-		p = subprocess.Popen('python chat.py', shell = True)
 		result = urllib2.urlopen("http://localhost:5000/")
-		#print result.code
 		self.assertEqual(result.code,200)
-		p.kill()
+		
 		
 
 	def setDown(self):
-		pass
+		p.kill()
+		
 		
 if __name__== '__main__':
 	unittest.main()			
