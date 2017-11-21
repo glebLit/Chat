5e3fa9f2b39f8ddf01672bb2c6a1830c70ef9168
Gleb <gleb19960@yandex.ru>
2017-11-21 23:09:10 +0300

    modifing test

--- test/test.py
+++ test/test.py
@@ -3,24 +3,20 @@ import unittest
 import urllib2
 import os
 import threading
-import subprocess
-from subprocess import PIPE,STDOUT
 from flask import Flask
+ 
 app = Flask(__name__)
 class FlaskTests(unittest.TestCase):
-	def setUp(self):
-		self.p = subprocess.Popen('python '+os.path.dirname(os.path.realpath(__file__))+"/../chat.py", shell = True, stdout = PIPE, stdin = PIPE, stderr = STDOUT)		
-		self.app = app.test_client()
-		self.app.testing = True
-		
-
-	def test_home_status_code(self):
-		result = urllib2.urlopen("http://localhost:5000/")
-		self.assertEqual(result.code,200)
-		
-	def setDown(self):
-		self.p.kill()
-		
-		
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+       
+    def test_home_status_code(self):
+        result = urllib2.urlopen("http://localhost:5000/")
+        self.assertEqual(result.code,200)
+       
+    def setDown(self):
+        pass       
+       
 if __name__== '__main__':
-	unittest.main()			
+    unittest.main()	
